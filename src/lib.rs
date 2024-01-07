d776c28ecdad37683f345410726cae3abd869030
Lord Divan <darveter667@gmail.com>
2024-01-07 15:07:37 +0300

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ const RESULT_EMOJII: [char; 2] = ['❌', '✅'];
 
 /// Checking if it possible to create file with given name in current directory
 /// based on it's length and length of absolute path with this file.
-pub fn is_file_len_valid(filename: &str) -> Result<bool, Box<dyn Error>> {
+pub fn is_file_len_valid_in_curr_dir(filename: &str) -> Result<bool, Box<dyn Error>> {
     if !utils::filename_is_unique(filename, &utils::get_current_working_dir()?)? {
         return Err(Box::from(format!(
             "File with name `{}` already exists",
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     match args.len() {
         0 => cfl::print_doc(),
         1 => {
-            let filename_is_valid =
-                cfl::is_file_len_valid(&args.next().expect("Filename argument expected"))?;
+            let filename_is_valid = cfl::is_file_len_valid_in_curr_dir(
+                &args.next().expect("Filename argument expected"),
+            )?;
             if !filename_is_valid {
                 cfl::exit_program(1)
             }
